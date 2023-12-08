e7fc8b2bd6bfda67958bf3ce1d2c83aa4aaa89d5
Susan L <136719807+Plushy3@users.noreply.github.com>
2023-12-08 20:45:42 +0000

    Update CFG-Final-Project.py
    Added processing of both Sales and Expenditure data

--- CFG-Final-Project.py
+++ CFG-Final-Project.py
@@ -15,9 +15,14 @@ data = read_data()
 sales = []
 expenditure = []
 month = []
-lowest = highest = 0
-low_month = " "
-high_month = " "
+
+lowest_sales = highest_sales = 0
+low_sales_month = " "
+high_sales_month = " "
+
+lowest_expenditure = highest_expenditure = 0
+low_exp_month = " "
+high_exp_month = " "
 
 
 def graph_print_one_bar(divide):
@@ -86,20 +91,38 @@ for row in data:
     # Find the Lowest and Highest Sales For the Year : Start by Defaulting The Lowest with the first months data
     # otherwise the initial zero value will always be the lowest amount
     my_month = str(row['month'])
-    if lowest == 0:
-        low_month = my_month.capitalize()
-        lowest = sale
-    elif lowest > sale:
-        low_month = my_month.capitalize()
-        lowest = sale
-    elif highest < sale:
-        high_month = my_month.capitalize()
-        highest = sale
+
+    if lowest_sales == 0:
+        low_sales_month = my_month.capitalize()
+        lowest_sales = sale
+    elif lowest_sales > sale:
+        low_sales_month = my_month.capitalize()
+        lowest_sales = sale
+    elif highest_sales < sale:
+        high_sales_month = my_month.capitalize()
+        highest_sales = sale
     sales.append(sale)
+
+    if lowest_expenditure == 0:
+        low_exp_month = my_month.capitalize()
+        lowest_expenditure = exp
+    elif lowest_expenditure > exp:
+        low_exp_month = my_month.capitalize()
+        lowest_expenditure = exp
+    elif highest_expenditure < exp:
+        high_exp_month = my_month.capitalize()
+        highest_expenditure = exp
+
+    expenditure.append(exp)
+    print('low_exp_month', low_exp_month)
+    print('lowest_expenditure', lowest_expenditure)
+    print('high_exp_month', high_exp_month)
+    print('highest_expenditure', highest_expenditure)
+    print('expenditure', expenditure)
 
 
 def graph_headings():
-    # Final Graph Heading and any calculated values ie. anything that goes on the graph that isnt the graph bars
+    # Final Graph Heading and any calculated values ie. anything that goes on the graph that isn't the graph bars
     turtle.color('black')
     turtle.penup()
     turtle.goto(100, 240)
@@ -114,19 +137,51 @@ def graph_headings():
     turtle.goto(0, -100)
     turtle.write(high_sales_mth)
     turtle.goto(0, -140)
-    turtle.write(avg_sales)
+    turtle.write(low_exp_mth)
     turtle.goto(0, -160)
+    turtle.write(high_exp_mth)
+
+    turtle.goto(0, -200)
+    turtle.write(avg_sales)
+    turtle.goto(0, -220)
+    turtle.write(avg_expenditure)
+
+    turtle.goto(0, -260)
     turtle.write(total_sales)
+    turtle.goto(0, -280)
+    turtle.write(total_expenditure)
+
+    turtle.goto(0, -320)
+    turtle.write(yearly_profit)
+
     # Hide the Arrow Head when the Graph is Completed
     turtle.hideturtle()
     turtle.done()
     return
 
+
 # Calculated Values used on the Graph within function graph_headings
-total = sum(sales)
-average = total // 12
-low_sales_mth = f"The lowest  sales value was in {low_month} this amount was £{lowest:,d}"
-high_sales_mth = f"The highest sales value was in {high_month} this amount was £{highest:,d}"
-avg_sales = f"Average sales per month: £{average:,d}"
-total_sales = f"Total sales: £{total:,d}"
+total_sales = sum(sales)
+total_expenditure = sum(expenditure)
+print('total_expenditure', total_expenditure)
+print('low_exp_mth', low_exp_month)
+yearly_profit = total_sales - total_expenditure
+
+average_sales = total_sales // 12
+average_expenditure = total_expenditure // 12
+
+low_sales_mth = f"The lowest  sales value was in {low_sales_month} this amount was £{lowest_sales:,d}"
+high_sales_mth = f"The highest sales value was in {high_sales_month} this amount was £{highest_sales:,d}"
+
+low_exp_mth = f"The lowest  expenditure value was in {low_exp_month} this amount was £{lowest_expenditure:,d}"
+high_exp_mth = f"The highest expenditure value was in {high_exp_month} this amount was £{highest_expenditure:,d}"
+
+avg_sales = f"Average sales per month: £{average_sales:,d}"
+avg_expenditure = f"Average expenditure per month: £{average_expenditure:,d}"
+
+total_sales = f"Total sales: £{total_sales:,d}"
+total_expenditure = f"Total expenditure: £{total_expenditure:,d}"
+
+yearly_profit = f"2018 Profit: £{yearly_profit:,d}"
+
 graph_headings()
