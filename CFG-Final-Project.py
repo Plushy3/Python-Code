c20ebec30cab79b312656ed5220f7351f9347fc2
Susan Ling <136719807+Plushy3@users.noreply.github.com>
2023-12-11 15:44:49 +0000

    Add files via upload

--- CFG-Final-Project.py
+++ CFG-Final-Project.py
@@ -11,21 +11,7 @@ def read_data():
     return data
 
 
-data = read_data()
-sales = []
-expenditure = []
-month = []
-
-lowest_sales = highest_sales = 0
-low_sales_month = " "
-high_sales_month = " "
-
-lowest_expenditure = highest_expenditure = 0
-low_exp_month = " "
-high_exp_month = " "
-
-
-def graph_print_one_bar(divide):
+def graph_print_one_bar(divide, color):
     # Fill Colour Inside the Graph Bar
     turtle.begin_fill()
     # Turn Turtle to Left to Go Up the Height of the Bar
@@ -54,7 +40,6 @@ def graph_print_one_bar(divide):
     turtle.end_fill()
     # Pen Down Ready to Draw Next Bar
     turtle.pendown()
-    return
 
 
 # Draw 1 Bar on Graph
@@ -65,8 +50,7 @@ def graph(sale, exp):
     turtle.color('black')
     # Next Line : Make the Graph Bar Smaller by Dividing the Excel Sales Data by 50
     divide = sale // 40
-
-    graph_print_one_bar(divide)
+    graph_print_one_bar(divide, 'black')
 
     # GRAPH BAR PRINT OF ONE MONTHS EXPENDITURE DATA
 
@@ -74,13 +58,65 @@ def graph(sale, exp):
     turtle.color('red')
     # Next Line : Make the Graph Bar Smaller by Dividing the Excel Expenses Data by 50
     divide = exp // 40
-
-    graph_print_one_bar(divide)
+    graph_print_one_bar(divide, 'red')
 
     # Leave a blank gap after each months graph bars
     turtle.penup()
     turtle.forward(11)
-    return
+
+
+def graph_headings(low_sales_mth, high_sales_mth, low_exp_mth, high_exp_mth, avg_sales, avg_expenditure,
+                   total_sales_str, total_expenditure_str, yearly_profit_str):
+    # Final Graph Heading and any calculated values ie. anything that goes on the graph that isn't the graph bars
+    turtle.color('black')
+    turtle.penup()
+    turtle.goto(100, 240)
+    turtle.write("Monthly Sales Figures - 2018")
+    turtle.goto(90, 230)
+    turtle.write("Sales (Black) / Expenditure (Red)")
+    turtle.goto(6, -20)
+    turtle.write("  J         F         M        A         M         J         J         A        "
+                 "S        O         N         D")
+    turtle.goto(0, -80)
+    turtle.write(low_sales_mth)
+    turtle.goto(0, -100)
+    turtle.write(high_sales_mth)
+    turtle.goto(0, -140)
+    turtle.write(low_exp_mth)
+    turtle.goto(0, -160)
+    turtle.write(high_exp_mth)
+
+    turtle.goto(0, -200)
+    turtle.write(avg_sales)
+    turtle.goto(0, -220)
+    turtle.write(avg_expenditure)
+
+    turtle.goto(0, -260)
+    turtle.write(total_sales_str)
+    turtle.goto(0, -280)
+    turtle.write(total_expenditure_str)
+
+    turtle.goto(0, -320)
+    turtle.write(yearly_profit_str)
+
+    # Hide the Arrow Head when the Graph is Completed
+    turtle.hideturtle()
+    turtle.done()
+
+
+data = read_data()
+sales = []
+expenditure = []
+month = []
+
+lowest_sales = highest_sales = 0
+lowest_expenditure = highest_expenditure = 0
+avg_sales = avg_expenditure = yearly_profit = 0
+
+low_sales_month = high_sales_month = ""
+low_exp_month = high_exp_month = ""
+lowest_exp_month = highest_exp_month = ""
+total_sales_str = total_expenditure_str = ""
 
 
 for row in data:
@@ -114,46 +150,6 @@ for row in data:
         highest_expenditure = exp
 
     expenditure.append(exp)
-    
-
-def graph_headings():
-    # Final Graph Heading and any calculated values ie. anything that goes on the graph that isn't the graph bars
-    turtle.color('black')
-    turtle.penup()
-    turtle.goto(100, 240)
-    turtle.write("Monthly Sales Figures - 2018")
-    turtle.goto(90, 230)
-    turtle.write("Sales (Black) / Expenditure (Red)")
-    turtle.goto(6, -20)
-    turtle.write("  J         F         M        A         M         J         J         A        "
-                 "S        O         N         D")
-    turtle.goto(0, -80)
-    turtle.write(low_sales_mth)
-    turtle.goto(0, -100)
-    turtle.write(high_sales_mth)
-    turtle.goto(0, -140)
-    turtle.write(low_exp_mth)
-    turtle.goto(0, -160)
-    turtle.write(high_exp_mth)
-
-    turtle.goto(0, -200)
-    turtle.write(avg_sales)
-    turtle.goto(0, -220)
-    turtle.write(avg_expenditure)
-
-    turtle.goto(0, -260)
-    turtle.write(total_sales)
-    turtle.goto(0, -280)
-    turtle.write(total_expenditure)
-
-    turtle.goto(0, -320)
-    turtle.write(yearly_profit)
-
-    # Hide the Arrow Head when the Graph is Completed
-    turtle.hideturtle()
-    turtle.done()
-    return
-
 
 # Calculated Values used on the Graph within function graph_headings
 total_sales = sum(sales)
@@ -165,16 +161,13 @@ average_expenditure = total_expenditure // 12
 
 low_sales_mth = f"The lowest  sales value was in {low_sales_month} this amount was £{lowest_sales:,d}"
 high_sales_mth = f"The highest sales value was in {high_sales_month} this amount was £{highest_sales:,d}"
-
 low_exp_mth = f"The lowest  expenditure value was in {low_exp_month} this amount was £{lowest_expenditure:,d}"
 high_exp_mth = f"The highest expenditure value was in {high_exp_month} this amount was £{highest_expenditure:,d}"
-
 avg_sales = f"Average sales per month: £{average_sales:,d}"
 avg_expenditure = f"Average expenditure per month: £{average_expenditure:,d}"
+total_sales_str = f"Total sales: £{total_sales:,d}"
+total_expenditure_str = f"Total expenditure: £{total_expenditure:,d}"
+yearly_profit_str = f"2018 Profit: £{yearly_profit:,d}"
 
-total_sales = f"Total sales: £{total_sales:,d}"
-total_expenditure = f"Total expenditure: £{total_expenditure:,d}"
-
-yearly_profit = f"2018 Profit: £{yearly_profit:,d}"
-
-graph_headings()
+graph_headings(low_sales_mth, high_sales_mth, low_exp_mth, high_exp_mth, avg_sales, avg_expenditure, total_sales_str,
+               total_expenditure_str, yearly_profit_str)
